de0ab4a5e5cab8d8c5aa4040784f2379b63cd767
GOD\Underpayed <ssxultimatehack@gmail.com>
2017-08-30 22:36:48 +0100

    Created empty Project

---
+++ Source/Game00/Game00.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Game00.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Game00, "Game00" );
---
+++ Source/Game00/Game00GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Game00GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAME00_API AGame00GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
